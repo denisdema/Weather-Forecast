e044a8895af1eb4a9ad0800ade984414bd034db4
denis <denisdema958@gmail.com>
2022-04-09 14:37:17 +0100

    added entities

--- src/main/java/entities/City.java
+++ src/main/java/entities/City.java
@@ -1,4 +1,25 @@
 package entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "city")
 public class City {
+    @Id
+    private int city_id;
+    private String name;
+
+    @OneToMany(mappedBy = "city")
+    private List<Weather> weatherList;
 }
--- src/main/java/entities/Weather.java
+++ src/main/java/entities/Weather.java
@@ -1,4 +1,26 @@
 package entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "weather")
 public class Weather {
+    @Id
+    private int weather_id;
+    private Date date;
+    private double humidity;
+    private double wind_speed;
+
+    @ManyToOne
+    @JoinColumn(name ="city_id" )
+    private City city;
+
 }
