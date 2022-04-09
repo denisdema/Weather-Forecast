18c2b8a5b43c618846155b04914df3d543c539bc
Eranio <eraniokashari@gmail.com>
2022-04-09 16:24:08 +0200

    git commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,18 @@
+import sun.text.resources.cldr.es.FormatData_es_419;
+
+import java.util.Date;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter name of city");
+        String qyteti= input.nextLine();
+        System.out.println("Enter the pattern of country ");
+        String weather= input.nextLine();
+        System.out.println("Enter date of the city for weather ");
+        int date = input.nextInt();
 
     }
 }
